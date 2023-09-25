e43b5dcd2c0be26de7fbbf874d3e0603322ee8de
DT0998 <duongkytrieuvn@gmail.com>
2023-09-25 22:08:38 +0700

    feat: do smth

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,9 +4,7 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpEventType,
 } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(
@@ -18,13 +16,6 @@ export class AuthInterceptorService implements HttpInterceptor {
       headers: req.headers.append('Auth', 'xyz'),
     });
     console.log('Request is on its way');
-    return next.handle(modifiedRequest).pipe(
-      tap((event) => {
-        if (event.type === HttpEventType.Response) {
-          console.log('Response arrived, body data: ');
-          console.log(event.body);
-        }
-      })
-    );
+    return next.handle(modifiedRequest);
   }
 }
